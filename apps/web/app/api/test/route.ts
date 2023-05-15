22ea9dab0d03112b7f9d60b7ad421f05d9214cd5
Maksym Cherniavskyi <morfey897@gmail.com>
2023-05-15 22:28:42 +0300

    Removed runtime

--- apps/web/app/api/test/route.ts
+++ apps/web/app/api/test/route.ts
@@ -1,6 +1,6 @@
 import type { NextRequest } from 'next/server';
 
-export const runtime = 'edge';
+// export const runtime = 'edge';
 
 export async function GET(req: NextRequest) {
   return new Response(
